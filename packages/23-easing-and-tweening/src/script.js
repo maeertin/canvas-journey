efe2f047e592e37f07ad77a78a63d6b183ab6e9b
Martin Barri <maeertin@gmail.com>
2022-05-29 18:30:21 +0200

    complete exercise 23

--- packages/23-easing-and-tweening/src/script.js
+++ packages/23-easing-and-tweening/src/script.js
@@ -20,25 +20,34 @@ const height = (canvas.height = window.innerHeight)
 /**
  * Config
  */
-const position = {
-  x: 0,
-  y: Math.random() * height,
-}
 const target = {
   x: width,
   y: Math.random() * height,
 }
-const ease = 0.1
+const numPoints = 100
+const points = Array.from(new Array(numPoints), () => ({
+  x: 0,
+  y: 0,
+}))
+const ease = 0.5
+let easing = true
 
 document.addEventListener('mousemove', (event) => {
   target.x = event.clientX
   target.y = event.clientY
+
+  if (!easing) {
+    easing = true
+    update()
+  }
 })
 
 update()
 
 function update() {
-  requestAnimationFrame(update)
+  if (easing) {
+    requestAnimationFrame(update)
+  }
 
   now = Date.now()
   elapsedTime = now - then
@@ -48,15 +57,32 @@ function update() {
   context.clearRect(0, 0, width, height)
 
   context.beginPath()
-  context.arc(position.x, position.y, 10, 0, Math.PI * 2)
-  context.fill()
-
-  const dx = target.x - position.x
-  const dy = target.y - position.y
-  position.x += dx * ease
-  position.y += dy * ease
-
-  context.beginPath()
   context.arc(target.x, target.y, 10, 0, Math.PI * 2)
   context.fill()
+
+  let leader = target
+  points.forEach((point) => {
+    context.beginPath()
+    context.arc(point.x, point.y, 10, 0, Math.PI * 2)
+    context.fill()
+
+    easeTo(point, leader, ease)
+    leader = point
+  })
+
+  easing = !points.every((point) => {
+    const dx = target.x - point.x
+    const dy = target.y - point.y
+    return Math.abs(dx) < 0.1 && Math.abs(dy) < 0.1
+  })
+
+  console.log('rafTick') // eslint-disable-line
+}
+
+function easeTo(p0, p1, amount) {
+  const dx = p1.x - p0.x
+  const dy = p1.y - p0.y
+
+  p0.x += dx * amount
+  p0.y += dy * amount
 }
